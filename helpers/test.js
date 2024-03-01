ef39a27f001ff7295e8d846e912feb880801b42a
jepajerowski <69466122+jepajerowski@users.noreply.github.com>
2024-03-01 17:01:57 -0500

    added to variables + utilities

--- helpers/test.js
+++ helpers/test.js
@@ -2,4 +2,8 @@ module.exports = function (Handlebars) {
   Handlebars.registerHelper('test', function () {
     return 'This is a test helper';
   });
+
+  Handlebars.registerHelper('toPx', function(spacerRem) {
+  return spacerRem * 16;
+});
 };
---
+++ source/_patterns/01-variables/03-layout-system/_layout-system.md
@@ -0,0 +1,4 @@
+---
+title: Layout System
+order: 2
+---
